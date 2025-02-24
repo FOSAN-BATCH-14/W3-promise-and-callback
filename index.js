1006cb699c70bc22451062e26a71ea8715b392c1
unknown <msyahrulmaajids@gmail.com>
2025-02-24 15:31:28 +0700

    w3-promise-and-callback-syahrul

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+const beli = require("./beli.js");
 
-//your Program in here
+console.log("Menjalankan program");
 
-console.log(`menjalankan program`)
+function mulaiBelanja(uang) {
+  beli(uang, { item: "Permen", harga: 2000, waktu: 2000 }, function (kembalian1) {
+    console.log("--------------------------------");
+    beli(kembalian1, { item: "Roti", harga: 3000, waktu: 3000 }, function (kembalian2) {
+      console.log("--------------------------------");
+      beli(kembalian2, { item: "Kue Sus", harga: 2500, waktu: 3000 }, function (kembalian3) {
+        console.log("--------------------------------");
+        beli(kembalian3, { item: "Susu", harga: 4000, waktu: 3000 }, function (kembalian4) {
+          console.log("--------------------------------");
+          beli(kembalian4, { item: "Coklat", harga: 1500, waktu: 3000 }, function () {
+            console.log("--------------------------------");
+          });
+        });
+      });
+    });
+  });
+}
+
+mulaiBelanja(10000);
--- program.js
+++ program.js
@@ -2,17 +2,41 @@ const fs = require('fs');
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
+  while (new Date().getTime() - start < milliseconds) {}
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
-
+  console.log("loading data parent.....");
+  sleep(2000);
+  fs.readFile(parent_file, 'utf8', (err, parentData) => {
+    if (err) {
+      console.error("Error reading parent file:", err);
+      return;
+    }
+    
+    const parents = JSON.parse(parentData);
+    
+    console.log("loading data children.....");
+    sleep(2000);
+    fs.readFile(children_file, 'utf8', (err, childrenData) => {
+      if (err) {
+        console.error("Error reading children file:", err);
+        return;
+      }
+      
+      const children = JSON.parse(childrenData);
+      
+      // Matching process
+      parents.forEach(parent => {
+        parent.children = children
+          .filter(child => child.family === parent.last_name)
+          .map(child => child.full_name);
+      });
+      
+      console.log(JSON.stringify(parents, null, 2));
+    });
+  });
 }
 
-match_data('./parents.json', './children.json')
+match_data('./parents.json', './children.json');
 console.log("Notification : Data sedang diproses !");
